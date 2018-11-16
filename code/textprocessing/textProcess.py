6162cde85bd1857c6228f7db80774254bdf1bc02
Rahul Chowdhury <rahulchowdhury@econ1-134-232-dhcp.int.colorado.edu>
2018-11-16 16:41:52 -0700

    added wodclouds for query type

--- code/textprocessing/textProcess.py
+++ code/textprocessing/textProcess.py
@@ -28,6 +28,8 @@ class textProcess:
             c = c+1
         #print(l)
         self.generate_word_cloud(l)
+
+
     def stop_words(self):
             s = "a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves"
             l = s.split()
@@ -46,18 +48,35 @@ class textProcess:
             filtered_words = list(filter(lambda word: word not in stop_words_list, clean_words)) #check for stop words
             #s = ''.join(filtered_words) #convert list to string
             return filtered_words
+    def query_type_wordcloud(self):
 
-    def generate_word_cloud(self,l):
+            collection = self.client[self.name_database][self.name_collection]
+            query_list = collection.distinct("query_keywords")
+            for q in query_list:
+                l = []
+                for post in collection.find({'query_keywords':q}): #iterate through each document
+                   #pprint.pprint(post['query_jobtyp_explvl'])
+
+                   jd = post['job_description']
+                   removed = self.removeStopWords(jd)
+                   l.extend(removed)
+                   #print(removed)
+
+                #print(l)
+                self.generate_word_cloud(l,q) #generate word cloud for every query type
+                print("done for ---",q)
+    def generate_word_cloud(self,l,q):
            #convert list to string and generate
            unique_string=(" ").join(l)
            wordcloud = WordCloud(width = 1000, height = 500).generate(unique_string)
            plt.figure(figsize=(15,8))
            plt.imshow(wordcloud)
            plt.axis("off")
-           plt.savefig("job_wordcloud"+".png", bbox_inches='tight')
-           plt.show()
+           plt.savefig(q+".png", bbox_inches='tight')
+           #plt.show()
            plt.close()
 
 if __name__ == "__main__":
             w = textProcess("mongodb://user:indeedjob@ec2-184-73-64-187.compute-1.amazonaws.com/appDatabase", "appDatabase", "jobs")
-            w.preprocess()
+            #w.preprocess()
+            w.query_type_wordcloud()
